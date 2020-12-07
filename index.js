10ef31a39c830580664714329619101375e20ab6
Michael Burkhardt <michael@peroxaan.com>
2020-12-07 18:03:04 -0500

    foundation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//test
+console.log(`Loading Bot...`)
+
+const fs = require(`fs`)
+const Discord = require(`discord.js`)
+const client = new Discord.Client()
+
+const config = JSON.parse(fs.readFileSync("./config.json"))
+
